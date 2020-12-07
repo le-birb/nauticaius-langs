94e9171f928bcfeb68ea5e39dfd222b54d6a0a0e
TheLordOfAllMuffins <nopika99@gmail.com>
2020-12-07 06:51:52 -0800

    Add pt romanization

--- karzi-talossian/talossian/pkt_to_pt.sh
+++ karzi-talossian/talossian/pkt_to_pt.sh
@@ -1,7 +1,9 @@
 
 rules=pt_rules
-romanization=
+romanization=pt_romanization
 classes=pt_classes
 sound_changer=../../../sound_changer.py
 
 python3 $sound_changer ../pkt_lex_stressed $rules -c $classes -o pt_changed
+
+python3 $sound_changer pt_changed $romanization -o pt_romanized
